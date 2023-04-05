82ac39d889c4da9ef12156e90413f6ced18a74f2
Bryan Ma <hello@bryan.ma>
2023-04-05 14:15:19 +0900

    First commit

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,18 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: look on my
+          works, ye mighty, and despair!
+        </p>
+      </>
+    </>
+  )
+}
+
+export default AboutPage
